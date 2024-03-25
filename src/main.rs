cd982682a96ddb2e9f390894c237006004d735d8
Graham Keenan <graham.keenan@outlook.com>
2024-03-25 16:53:13 +0000

    wired up stdin and stderr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use anyhow::{Context, Result};
 
 use clap::{Args, Parser, Subcommand};
@@ -28,33 +30,33 @@ struct RunArgs {
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    println!("Logs from your program will appear here!");
     let cli = Cli::parse();
 
     match cli.command {
         Commands::Run(args) => {
-            println!("The run args: {args:?}");
+            let command = args.command;
+            let command_args = args.args;
+
+            let exec = std::process::Command::new(&command)
+                .args(&command_args)
+                .output()
+                .with_context(|| {
+                    format!(
+                        "Running command {:?} with arguments {:?}",
+                        &command, &command_args
+                    )
+                })?;
+
+            let mut stdout = std::io::stdout();
+            let mut stderr = std::io::stderr();
+            stdout
+                .write_all(&exec.stdout)
+                .context("command stdout response")?;
+            stderr
+                .write(&exec.stderr)
+                .context("command stderr output")?;
         }
     }
-    //let args: Vec<_> = std::env::args().collect();
-    //let command = &args[3];
-    //let command_args = &args[4..];
-    //let output = std::process::Command::new(command)
-    //    .args(command_args)
-    //    .output()
-    //    .with_context(|| {
-    //        format!(
-    //            "Tried to run '{}' with arguments {:?}",
-    //            command, command_args
-    //        )
-    //    })?;
-
-    //if output.status.success() {
-    //    let std_out = std::str::from_utf8(&output.stdout)?;
-    //    println!("{}", std_out);
-    //} else {
-    //    std::process::exit(1);
-    //}
 
     Ok(())
 }

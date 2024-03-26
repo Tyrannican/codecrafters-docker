3a580ec132ca18ecf4b3fae9704ab39ceb54f3bb
Graham Keenan <graham.keenan@outlook.com>
2024-03-26 18:45:50 +0000

    authentication to docker registry

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use clap::{Args, Parser, Subcommand};
 
+mod image;
 mod runner;
 
 #[derive(Parser, Debug)]
@@ -13,6 +14,7 @@ pub(crate) struct Cli {
 #[derive(Subcommand, Debug)]
 pub(crate) enum Commands {
     Run(ExecArgs),
+    Image(DebugArgs),
 }
 
 #[derive(Args, Debug)]
@@ -27,11 +29,22 @@ pub(crate) struct ExecArgs {
     args: Vec<String>,
 }
 
+#[derive(Args, Debug)]
+pub(crate) struct DebugArgs {
+    /// Image to run
+    image: String,
+}
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match cli.command {
         Commands::Run(args) => runner::run_command(&args.command, &args.args),
+        Commands::Image(args) => {
+            let is = image::ImageService::new(&args.image)?;
+            println!("Image: {}, Auth: {}", is.image, is.auth.token);
+            Ok(())
+        }
     }
 }
